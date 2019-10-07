dd0db1418b811c4d9586591c0129a3fab67a9bcd
Bartek Pacia <barpac02@gmail.com>
2019-10-07 18:32:55 +0200

    added CameraX deps and created empty ImageAnalyzer class

---
+++ app/src/main/java/pl/baftek/hackheroes_2019/ImageAnalyzer.kt
@@ -0,0 +1,10 @@
+package pl.baftek.hackheroes_2019
+
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+
+class ImageAnalyzer : ImageAnalysis.Analyzer {
+    override fun analyze(image: ImageProxy?, rotationDegrees: Int) {
+        // TODO
+    }
+}
