a97df14075c3e1f8c88f1bf45a17abefa96d546c
Bartek Pacia <barpac02@gmail.com>
2019-10-07 18:45:03 +0200

    added requesting camera permission

--- app/src/main/java/pl/baftek/hackheroes_2019/MainActivity.kt
+++ app/src/main/java/pl/baftek/hackheroes_2019/MainActivity.kt
@@ -1,12 +1,73 @@
 package pl.baftek.hackheroes_2019
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+private const val REQUEST_CODE_CAMERA_PERMISSION = 10
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Request camera permissions
+        if (isCameraAccessGranted()) {
+            viewfinder.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.CAMERA), REQUEST_CODE_CAMERA_PERMISSION
+            )
+        }
+
+
+        // Every time the provided texture view changes, recompute layout
+        viewfinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
+    }
+
+    /**
+     * Process result from permission request dialog box, has the request
+     * been granted? If yes, start Camera. Otherwise display a toast
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_CAMERA_PERMISSION) {
+            if (isCameraAccessGranted()) {
+                viewfinder.post { startCamera() }
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user.",
+                    LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    /**
+     * Check if all permission specified in the manifest have been granted
+     */
+    private fun isCameraAccessGranted(): Boolean {
+        return ContextCompat
+            .checkSelfPermission(baseContext, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startCamera() {
+        Toast.makeText(this, "camera started", LENGTH_SHORT).show()
+    }
+
+    private fun updateTransform() {
+
     }
 }
